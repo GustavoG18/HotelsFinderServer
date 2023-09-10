caaf90dec873e92f571ce354df3144c2ff355ffb
GustavoG18 <gguerrerof98@gmail.com>
2023-09-10 13:34:57 -0500

    add endpoint to get users.

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createUser, createUserAdmin } from "../controllers/user.controller.js";
+import {
+  createUser,
+  createUserAdmin,
+  getUser,
+} from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.post("/createUser", createUser);
 router.post("/createUserAdmin", createUserAdmin);
+router.get("/getUser", getUser);
 
 export default router;
